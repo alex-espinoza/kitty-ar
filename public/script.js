3359fa457b657f2d2f4d711dfb239ec2d2d55fa1
Alex Espinoza <aespinoza@aspria.net>
2018-05-17 12:20:49 -0400

    Create kitty loader overlay

--- public/script.js
+++ public/script.js
@@ -1,3 +1,13 @@
 $(function() {
-  console.log("awesome");
+  $kittyLoaderOverlay = $('.window__kitty-loader-overlay');
+  $loadSceneButton = $('.js-load-scene');
+
+  $loadSceneButton.on('click', function() {
+    $kittyLoaderOverlay.addClass('hide');
+  });
+
+  document.querySelector('a-scene').addEventListener('loaded', function () {
+    $loadSceneButton.prop('disabled', false);
+    $loadSceneButton.text('Launch scene');
+  });
 });
