7d45560e3d3742fb0d908f35181616f98b8d15a1
Alex Espinoza <aespinoza@aspria.net>
2018-05-21 20:32:32 -0400

    Handing building kitty image data URI on API instead of client side

--- public/script.js
+++ public/script.js
@@ -5,45 +5,26 @@ $(function() {
   var $aFrameAssets = document.querySelector('a-assets');
 
   $launchSceneButton.on('click', async function() {
-    var kittyImageBase64 = await getKittyImageBase64ById(1);
+    var kittyId = 12345;
+    var kittyImageData = await getKittyImageDataById(kittyId);
 
-    if (!$.isEmptyObject(kittyImageBase64) || kittyImageBase64 != null) {
+    if (kittyImageData.pngData !== null || kittyImageData.svgData !== null) {
       console.log('kitty image was successfully found and fetched');
-      await saveKittyImageBase64ToLocalStorage(kittyImageBase64);
-      await loadKittyImageFromBase64(kittyImageBase64);
+      await saveKittyImageDataToLocalStorage(kittyId, kittyImageData);
+      await loadKittyImageFromLocalStorage(kittyId);
 
       $kittyLoaderOverlay.hide();
     } else {
       console.log('kitty image could not be found');
+      // handle showing error on screen here
     }
   });
 
-  async function getKittyImageBase64ById(kittyId) {
-    var kittyImageExtensions = ['.png', '.svg'];
-    var kittyImageBase64 = {};
+  async function getKittyImageDataById(kittyId) {
+    var kittyImageData;
+    var kittyImageDataEndpoint = `https://wt-f8af72159b229d5a895848a643ddf7bf-0.sandbox.auth0-extend.com/kitty-image-to-base64?kittyId=${kittyId}`;
 
-    for (var i = 0; i < kittyImageExtensions.length; i++) {
-      console.log('checking for kitty image with extension:', kittyImageExtensions[i]);
-      var extension = kittyImageExtensions[i];
-      kittyImageBase64 = await checkIfKittyImageExists(kittyId, extension);
-
-      if (!$.isEmptyObject(kittyImageBase64)) {
-        return kittyImageBase64;
-      }
-    }
-
-    // kitty image not found for either .png or .svg
-    return {};
-  }
-
-  async function checkIfKittyImageExists(kittyId, extension) {
-    var kittyImageBase64 = '';
-    var imageUrlToBase64Endpoint = 'https://wt-f8af72159b229d5a895848a643ddf7bf-0.sandbox.auth0-extend.com/image-url-to-base64?imageUrl=';
-    var imageStorageUrl = 'https://storage.googleapis.com/ck-kitty-image/0x06012c8cf97bead5deae237070f9587f8e7a266d/';
-    var imageStorageRequestUrl = imageStorageUrl + kittyId + extension;
-    var requestUrl = imageUrlToBase64Endpoint + imageStorageRequestUrl;
-
-    await fetch(requestUrl)
+    await fetch(kittyImageDataEndpoint)
       .then(function(response) {
         if (!response.ok) {
           throw Error(response);
@@ -52,33 +33,35 @@ $(function() {
         return response.json();
       })
       .then(function(json) {
-        console.log(extension, '200 success when fetching kitty url: ', json);
-        kittyImageBase64 = { data: json.data, extension: extension, kittyId: kittyId };
+        console.log(`200 success when fetching kitty ${kittyId} image data:`, json);
+        kittyImageData = json;
       })
       .catch(function(error) {
-        console.log(extension, 'there was an error when fetching kitty url: ', error);
-        kittyImageBase64 = {};
+        console.log(`there was an error when fetching kitty ${kittyId} image data:`, error);
+        kittyImageData = {
+          pngData: null,
+          svgData: null
+        };
       });
 
-    return kittyImageBase64;
+    return kittyImageData;
   }
 
-  function saveKittyImageBase64ToLocalStorage(base64Object) {
-    var kittyItem = 'kitty-' + base64Object.kittyId;
-    var kittyDataUri;
+  function saveKittyImageDataToLocalStorage(kittyId, kittyImageData) {
+    var kittyItem = 'kitty-' + kittyId;
 
-    if (base64Object.extension == '.png') {
-      kittyDataUri = 'data:image/png;base64,' + base64Object.data;
+    if (kittyImageData.pngData !== null) {
+      kittyDataUrl = kittyImageData.pngData;
     } else {
-      kittyDataUri = 'data:image/svg+xml;base64,' + base64Object.data;
+      kittyDataUrl = kittyImageData.svgData;
     }
 
-    localStorage.setItem(kittyItem, kittyDataUri);
+    localStorage.setItem(kittyItem, kittyDataUrl);
     console.log(kittyItem, 'has been saved to localStorage');
   }
 
-  function loadKittyImageFromBase64(base64Object) {
-    var kittyItem = 'kitty-' + base64Object.kittyId;
+  function loadKittyImageFromLocalStorage(kittyId) {
+    var kittyItem = 'kitty-' + kittyId;
     var newKittyImageElement = document.createElement('img');
 
     newKittyImageElement.setAttribute('id', 'kitty-from-storage');
