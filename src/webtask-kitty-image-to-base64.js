10ba1a0bb602ec713ea2517354792499fec7dd2b
Alex Espinoza <aespinoza@aspria.net>
2018-05-23 10:57:02 -0400

    Update webtask API to hit the official CryptoKitties API and return more kitty data

--- src/webtask-kitty-image-to-base64.js
+++ src/webtask-kitty-image-to-base64.js
@@ -6,57 +6,64 @@ import { fromExpress } from 'webtask-tools';
 import bodyParser from 'body-parser';
 import base64 from 'node-base64-image';
 import { promisify } from 'util';
+import fetch from 'node-fetch';
 
 bodyParser.urlencoded();
-
-var app = express();
+const encode = promisify(base64.encode);
+const CRYPTO_KITTIES_API_URL = 'https://api.cryptokitties.co/kitties';
+const app = express();
 app.use(bodyParser.urlencoded());
 
-var CRYPTO_KITTIES_IMAGE_STORAGE_URL = 'https://storage.googleapis.com/ck-kitty-image/0x06012c8cf97bead5deae237070f9587f8e7a266d/';
-
-var encode = promisify(base64.encode);
-
 app.get('/', async (request, response) => {
-  var kittyId = request.query.kittyId;
-  var kittyData = {
+  const kittyId = request.query.kittyId;
+
+  let kittyData = {
     id: kittyId,
-    pngUrl: `${CRYPTO_KITTIES_IMAGE_STORAGE_URL}${kittyId}.png`,
-    pngData: null,
-    svgUrl: `${CRYPTO_KITTIES_IMAGE_STORAGE_URL}${kittyId}.svg`,
-    svgData: null
+    name: null,
+    bio: null,
+    imageUrl: null,
+    imageExtension: null,
+    imageData: null,
   };
-  var base64Options = { string: true };
 
   try {
-    console.log('attempting to get png data');
-    var pngImage = await encode(kittyData.pngUrl, base64Options);
-    console.log('got data from png');
-    kittyData.pngData = await formatBase64ToDataUrl(pngImage, 'png');
+    console.log('Attempting to get data from CryptoKitties API...');
+    const response = await fetch(`${CRYPTO_KITTIES_API_URL}/${kittyId}`);
+    const responseJson = await response.json();
+    saveAPIData(kittyData, responseJson);
+    console.log('Successfully received data from API!');
 
   } catch (error) {
-    console.log('png not found', error);
+    console.log('API returned an error:', error);
+    return response.json(kittyData);
   }
 
-  if (kittyData.pngData === null) {
-    try {
-      console.log('attempting to get svg data');
-      var svgImage = await encode(kittyData.svgUrl, base64Options);
-      console.log('got data from svg');
-      kittyData.svgData = formatBase64ToDataUrl(svgImage, 'svg');
+  try {
+    const base64Options = { string: true };
+    console.log('Attempting to get base64 data from kitty image...');
+    const base64 = await encode(kittyData.imageUrl, base64Options);
+    console.log('Successfully encoded base64 data from kitty image!');
+    kittyData.imageData = formatBase64ToDataUrl(base64, kittyData.imageExtension);
 
-    } catch (error) {
-      console.log('svg not found', error);
-    }
+  } catch (error) {
+    console.log('Kitty image could not be encoded:', error);
   }
 
   return response.json(kittyData);
 });
 
-function formatBase64ToDataUrl(base64String, extension) {
+function saveAPIData(kittyData, responseJson) {
+  kittyData.name = responseJson.name;
+  kittyData.bio = responseJson.bio;
+  kittyData.imageUrl = responseJson.image_url_cdn;
+  kittyData.imageExtension = responseJson.image_url_cdn.split('.').pop();
+}
+
+function formatBase64ToDataUrl(base64, extension) {
   if (extension === 'png') {
-    return `data:image/png;base64,${base64String}`;
+    return `data:image/png;base64,${base64}`;
   } else {
-    return `data:image/svg+xml;base64,${base64String}`;
+    return `data:image/svg+xml;base64,${base64}`;
   }
 }
 
