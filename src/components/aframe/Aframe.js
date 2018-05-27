0937c867ea4091b41c31844ac6ed3af094ca8b24
Alex Espinoza <aespinoza@aspria.net>
2018-05-27 16:41:45 -0400

    Add transparent pixel to Aframe component

--- src/components/aframe/Aframe.js
+++ src/components/aframe/Aframe.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './Aframe.css';
+import transparentPixel from './transparent-1px.png'
 
 class Aframe extends React.Component {
   render() {
     return (
       <a-scene embedded kitty-scene arjs="sourceType: webcam; debugUIEnabled: false;">
         <a-assets>
-          <img id="transparent-pixel" src="./transparent-1px.png" />
+          <img id="transparent-pixel" src={transparentPixel} />
         </a-assets>
 
         <a-image id="kittyImage" src="#transparent-pixel" width="3" height="3" position="0 0.5 0" rotation="0 0 0"></a-image>
