62f7c04683e9b312676fe48fc0b07b3240586a60
Alex Espinoza <aespinoza@aspria.net>
2018-05-27 16:41:32 -0400

    Aframe loaded logic added to KittyLoader component

--- src/components/kitty-loader/KittyLoader.js
+++ src/components/kitty-loader/KittyLoader.js
@@ -11,6 +11,29 @@ class KittyLoader extends React.Component {
     };
   }
 
+  componentDidMount() {
+    setTimeout(() => {
+      this.checkIfARLoaded();
+    }, 3000);
+  }
+
+  checkIfARLoaded() {
+    let checkIfARLoaded = setInterval(() => {
+      let aSceneLoaded = document.getElementsByTagName('a-scene')[0].hasChildNodes();
+      let aAssetsLoaded = document.getElementsByTagName('a-assets')[0].hasChildNodes();
+      let aImageLoaded = document.getElementsByTagName('a-image')[0];
+      let canvasLoaded = document.getElementsByTagName('canvas')[0];
+      let videoLoaded = document.getElementsByTagName('video')[0];
+
+      if (aSceneLoaded && aAssetsLoaded && aImageLoaded && canvasLoaded && videoLoaded) {
+        this.setState({
+          splashScreen: false
+        });
+        clearInterval(checkIfARLoaded);
+      }
+    }, 1000);
+  }
+
   render() {
     const { splashScreen } = this.state;
 
