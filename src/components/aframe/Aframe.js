74dfa83fddd88e3ced36d5cbae506d434130319d
Alex Espinoza <aespinoza@aspria.net>
2018-05-27 14:51:12 -0400

    Start structuring React app, create Aframe component

---
+++ src/components/aframe/Aframe.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Aframe.css';
+
+class Aframe extends React.Component {
+  render() {
+    return (
+      <a-scene embedded kitty-scene arjs="sourceType: webcam; debugUIEnabled: false;">
+        <a-assets>
+          <img id="transparent-pixel" src="./transparent-1px.png" />
+        </a-assets>
+
+        <a-image id="kittyImage" src="#transparent-pixel" width="3" height="3" position="0 0.5 0" rotation="0 0 0"></a-image>
+        <a-marker-camera preset="hiro"></a-marker-camera>
+      </a-scene>
+    )
+  }
+}
+
+export default Aframe;
