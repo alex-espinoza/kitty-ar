448e35b8abfdae0a7fe2198636c5396226cb9dd8
Alex Espinoza <aespinoza@aspria.net>
2018-05-29 12:49:15 -0400

    Manually manipulating the DOM elements is the only way I can do this, and an OK way to do it as per the A-Frame docs. A-Frame performs better this way, outside of being rendered by React, anyways

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import KittyLoader from './components/kitty-loader/KittyLoader';
-import AFRAME from 'aframe';
-import 'aframe-state-component';
+import 'aframe';
 import './App.css';
 
 class App extends React.Component {
   constructor() {
     super();
+    this.loadARjs();
 
     this.state = {
       showKittyLoader: true
     };
 
-    this.registerAframeState();
-    this.loadARjs();
     this.handleSelectKittyButton = this.handleSelectKittyButton.bind(this);
   }
 
@@ -22,22 +20,6 @@ class App extends React.Component {
     arjsScript.src = 'https://cdn.rawgit.com/jeromeetienne/AR.js/1.6.0/aframe/build/aframe-ar.js';
     arjsScript.async = true;
     document.body.appendChild(arjsScript);
-  }
-
-  registerAframeState() {
-    AFRAME.registerState({
-      initialState: {
-        kittyImageData: '#transparent-pixel'
-        //kittyImageAssetId: '#transparent-pixel'
-      },
-
-      handlers: {
-        setKittyImageData: function(state, action) {
-          state.kittyImageData = action.kittyImageData;
-          //state.kittyImageAssetId = '#kitty-from-storage';
-        }
-      }
-    });
   }
 
   handleSelectKittyButton(kittyId) {
@@ -51,8 +33,7 @@ class App extends React.Component {
   loadSelectedKittyFromLocalStorage(kittyId) {
     let kittyItem = `kitty-${kittyId}`;
     let kittyImageData = JSON.parse(localStorage.getItem(kittyItem)).imageData;
-    AFRAME.scenes[0].emit('setKittyImageData', {kittyImageData: kittyImageData});
-    //document.querySelector('#kitty-from-storage').setAttribute('src', kittyImageData);
+    document.querySelector('#kittyImage').setAttribute('src', kittyImageData);
   }
 
   render() {
