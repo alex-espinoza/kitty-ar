a83ab95a2384cd6ed05825a114aec228aa06d17e
Alex Espinoza <aespinoza@aspria.net>
2018-06-12 14:05:52 -0400

    Update splash screen text

--- src/components/kitty-loader/SplashScreen.js
+++ src/components/kitty-loader/SplashScreen.js
@@ -6,7 +6,7 @@ const SplashScreen = () => {
     <div className="SplashScreen">
       <h1 className="SplashScreen-header">Kitty AR</h1>
       <p className="SplashScreen-paragraph">Bring your CryptoKitties to life with augmented reality!</p>
-      <p className="SplashScreen-paragraph">Please allow us your phone's camera access to work our magic!</p>
+      <p className="SplashScreen-paragraph">Please allow us to access your phone's camera to work our magic!</p>
       <div className="SplashScreen-diamond">
         <div className="SplashScreen-diamond-pseudo"></div>
       </div>
